3995815f65e08908ef6fe92182d2520c35bcd736
Denys Harkavenko <garkavenkodenispost@gmail.com>
2024-04-06 02:32:16 +0200

    fix problem with props

--- src/components/Main/Modules/ExperienceStepper.tsx
+++ src/components/Main/Modules/ExperienceStepper.tsx
@@ -1,59 +1,66 @@
 import {
-  Box,
-  Step,
-  StepDescription,
-  StepIcon,
-  StepIndicator,
-  StepSeparator,
-  StepStatus,
-  StepTitle,
-  Stepper,
-  useSteps,
+	Box,
+	Step,
+	Text,
+	StepDescription,
+	StepIcon,
+	StepIndicator,
+	StepSeparator,
+	StepStatus,
+	StepTitle,
+	Stepper,
+	useSteps,
 } from '@chakra-ui/react'
 import { steps } from '../../../data/experience'
 
 const ExperienceStepper: React.FC = () => {
-  const { activeStep } = useSteps({
-    index: 1,
-    count: steps.length,
-  })
+	const { activeStep } = useSteps({
+		index: 1,
+		count: steps.length,
+	})
 
-  return (
-    <Stepper
-      index={activeStep}
-      orientation='vertical'
-      height='390px'
-      gap='0'
-      colorScheme='gray'
-      size='sm'
-      mt='20px'
-    >
-      {steps.map((step, index) => (
-        <Step key={index}>
-          <StepIndicator bg='#fff'>
-            <StepStatus complete={<StepIcon />} />
-          </StepIndicator>
-
-          <Box flexShrink='0'>
-            <StepTitle color='#fff' fontWeight='bold' fontSize='20px'>
-              {step.title}
-            </StepTitle>
-            <StepDescription fontSize='16px' color='#fff' fontWeight='medium'>
-              {step.company}
-            </StepDescription>
-            <StepDescription fontSize='16px' as='div'>
-              {step.description}
-            </StepDescription>
-            <StepDescription color='#94ADC7' fontSize='14px'>
-              {step.time}
-            </StepDescription>
-          </Box>
-
-          <StepSeparator />
-        </Step>
-      ))}
-    </Stepper>
-  )
+	return (
+		<Stepper
+			index={activeStep}
+			orientation='vertical'
+			height='390px'
+			gap='0'
+			colorScheme='gray'
+			size='sm'
+			mt='20px'
+		>
+			{steps.map((step, index) => (
+				<Step key={index}>
+					<StepIndicator bg='#fff'>
+						<StepStatus complete={<StepIcon />} />
+					</StepIndicator>
+					<Box flexShrink='0'>
+						<StepTitle>
+							<Text color='#fff' fontWeight='bold' fontSize='20px'>
+								{step.title}
+							</Text>
+						</StepTitle>
+						<StepDescription>
+							<Text fontSize='16px' color='#fff' fontWeight='medium'>
+								{step.company}
+							</Text>
+						</StepDescription>
+						<StepDescription>
+							<Text fontSize='16px' as='div'>
+								{step.description}
+							</Text>
+						</StepDescription>
+						<StepDescription>
+							<Text color='#94ADC7' fontSize='14px'>
+								{step.time}
+							</Text>
+						</StepDescription>
+					</Box>
+					<StepSeparator />
+				</Step>
+			))}
+		</Stepper>
+	)
 }
 
 export default ExperienceStepper
