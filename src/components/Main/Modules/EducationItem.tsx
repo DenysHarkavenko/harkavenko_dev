f39ad7f5a6b2de30dbe9ef493ef128e5d9c47fa2
Denys Harkavenko <garkavenkodenispost@gmail.com>
2024-04-15 03:44:59 +0200

    fix no-full size education card

--- src/components/Main/Modules/EducationItem.tsx
+++ src/components/Main/Modules/EducationItem.tsx
@@ -1,53 +1,52 @@
 import { Link, Flex, Heading, Box, Image, Text } from '@chakra-ui/react'
 
 const EducationItem: React.FC<Education> = ({
-  title,
-  description,
-  img,
-  time,
-  link,
+	title,
+	description,
+	img,
+	time,
+	link,
 }) => {
-  return (
-    <Box maxW='450px'>
-      <Link
-        _hover={{
-          textDecoration: 'none',
-        }}
-        href={link}
-        target='_blank'
-      >
-        <Flex
-          mt='15px'
-          bg='#1A2633'
-          maxW='450px'
-          minH='133px'
-          direction={['column', 'row', 'row', 'row', 'row']}
-          borderRadius='12px'
-          p='15px 10px'
-          align='center'
-          justify={['space-between', null, 'unset', null, null]}
-          m={['15px auto', null, '15px 0', null, null]}
-          _hover={{
-            transform: 'scale(103%)',
-          }}
-          transition='0.2s all linear'
-        >
-          <Image src={img} w='100px' />
-          <Box ml='15px'>
-            <Heading fontSize='20px' mt={['15px', 'unset', null, null, null]}>
-              {title}
-            </Heading>
-            <Text fontSize='16px' m='5px 0'>
-              {description}
-            </Text>
-            <Text fontSize='14px' color='#94ADC7'>
-              {time}
-            </Text>
-          </Box>
-        </Flex>
-      </Link>
-    </Box>
-  )
+	return (
+		<Box maxW='445px' w='100%'>
+			<Link
+				w='100%'
+				_hover={{
+					textDecoration: 'none',
+				}}
+				href={link}
+				target='_blank'
+			>
+				<Flex
+					bg='#1A2633'
+					w='100%'
+					minH='133px'
+					direction={['column', 'row', 'row', 'row', 'row']}
+					borderRadius='12px'
+					p='15px 10px'
+					align='center'
+					justify={['space-between', null, 'unset', null, null]}
+					_hover={{
+						transform: 'scale(102%)',
+					}}
+					transition='0.2s all linear'
+				>
+					<Image src={img} w='100px' />
+					<Box ml='15px'>
+						<Heading fontSize='20px' mt={['15px', 'unset', null, null, null]}>
+							{title}
+						</Heading>
+						<Text fontSize='16px' m='5px 0'>
+							{description}
+						</Text>
+						<Text fontSize='14px' color='#94ADC7'>
+							{time}
+						</Text>
+					</Box>
+				</Flex>
+			</Link>
+		</Box>
+	)
 }
 
 export default EducationItem
