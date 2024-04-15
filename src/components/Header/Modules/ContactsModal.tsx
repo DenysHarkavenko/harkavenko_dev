ab5ec61ddb155ad90d88a9a38faa017c7a46d99c
Denys Harkavenko <garkavenkodenispost@gmail.com>
2024-04-15 03:50:14 +0200

    upd header buttons dynamic

--- src/components/Header/Modules/ContactsModal.tsx
+++ src/components/Header/Modules/ContactsModal.tsx
@@ -1,80 +1,81 @@
 import {
-  Button,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-  useDisclosure,
-  Link,
-  Flex,
+	Button,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalCloseButton,
+	ModalBody,
+	ModalFooter,
+	useDisclosure,
+	Link,
+	Flex,
 } from '@chakra-ui/react'
 
 const ContactsModal: React.FC = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  return (
-    <>
-      <Button
-        onClick={onOpen}
-        w='131px'
-        borderRadius='12px'
-        p='8px 0'
-        textAlign='center'
-        bg='#243647'
-        color='#fff'
-        ml='12px'
-        _hover={{ transform: 'scale(108%)' }}
-      >
-        Contact Me
-      </Button>
+	const { isOpen, onOpen, onClose } = useDisclosure()
+	return (
+		<>
+			<Button
+				onClick={onOpen}
+				w='131px'
+				borderRadius='12px'
+				p='8px 0'
+				textAlign='center'
+				bg='#243647'
+				color='#fff'
+				ml='12px'
+				_hover={{ transform: 'scale(105%)' }}
+				transition='0.2s all linear'
+			>
+				Contact Me
+			</Button>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent bg='#121921' borderRadius='12px' m='60px 10px'>
-          <ModalHeader color='#fff' fontWeight='bold'>
-            Contact me
-          </ModalHeader>
-          <ModalCloseButton color='#fff' />
-          <ModalBody color='#fff'>
-            <Flex direction='column'>
-              <Link fontWeight='bold' href='https://t.me/de0nn1s'>
-                Telegram
-              </Link>
-              <Link
-                fontWeight='bold'
-                mt='10px'
-                href='https://github.com/DenysHarkavenko'
-              >
-                GitHub
-              </Link>
-              <Link
-                fontWeight='bold'
-                mt='10px'
-                href='https://www.linkedin.com/in/denys-harkavenko-52234a251/'
-              >
-                LinkedIn
-              </Link>
-              <Link
-                fontWeight='bold'
-                mt='10px'
-                href='mailto: harkavenkodev@gmail.com'
-              >
-                harkavenkodev@gmail.com
-              </Link>
-            </Flex>
-          </ModalBody>
+			<Modal isOpen={isOpen} onClose={onClose}>
+				<ModalOverlay />
+				<ModalContent bg='#121921' borderRadius='12px' m='60px 10px'>
+					<ModalHeader color='#fff' fontWeight='bold'>
+						Contact me
+					</ModalHeader>
+					<ModalCloseButton color='#fff' />
+					<ModalBody color='#fff'>
+						<Flex direction='column'>
+							<Link fontWeight='bold' href='https://t.me/de0nn1s'>
+								Telegram
+							</Link>
+							<Link
+								fontWeight='bold'
+								mt='10px'
+								href='https://github.com/DenysHarkavenko'
+							>
+								GitHub
+							</Link>
+							<Link
+								fontWeight='bold'
+								mt='10px'
+								href='https://www.linkedin.com/in/denys-harkavenko-52234a251/'
+							>
+								LinkedIn
+							</Link>
+							<Link
+								fontWeight='bold'
+								mt='10px'
+								href='mailto: harkavenkodev@gmail.com'
+							>
+								harkavenkodev@gmail.com
+							</Link>
+						</Flex>
+					</ModalBody>
 
-          <ModalFooter>
-            <Button colorScheme='blue' onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
-  )
+					<ModalFooter>
+						<Button colorScheme='blue' onClick={onClose}>
+							Close
+						</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
+	)
 }
 
 export default ContactsModal
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,23 +2,23 @@ import { Box } from '@chakra-ui/react'
 import AboutMe from './Sections/AboutMe'
 import DevelopmentExperience from './Sections/DevelopmentExperience'
 import Skills from './Sections/Skills'
-import Projects from './Sections/Projects'
+// import Projects from './Sections/Projects'
 import LifeStory from './Sections/LifeStory'
 import Education from './Sections/Education'
 
 const Main: React.FC = () => {
-  return (
-    <Box as='main' bg='#121921'>
-      <Box maxW='980px' w='100%' margin='0 auto'>
-        <AboutMe />
-        <DevelopmentExperience />
-        <Education />
-        <Skills />
-        <Projects />
-        <LifeStory />
-      </Box>
-    </Box>
-  )
+	return (
+		<Box as='main' bg='#121921'>
+			<Box maxW='980px' w='100%' margin='0 auto'>
+				<AboutMe />
+				<DevelopmentExperience />
+				<Education />
+				<Skills />
+				{/* <Projects /> */}
+				<LifeStory />
+			</Box>
+		</Box>
+	)
 }
 
 export default Main
